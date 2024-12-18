4823f0ab70810aed12621d7f6b508571540ec64c
Gavin Salcedo <gavilan.salcedo>
2024-12-18 10:12:14 +0800

    update description and add JobCards component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useState } from 'react'
-import CardContainer from './components/CardContainer'
-import Tags from './components/Tags'
+import JobCards from './components/JobCards'
 
 export default function Home() {
   const [activeLink, setActiveLink] = useState(0)
@@ -55,68 +54,55 @@ export default function Home() {
         <section>
           <div className="font-semibold text-slate-500 text-justify">
             <p className="mb-4">
-              Back in 2012, I decided to try my hand at creating custom Tumblr
-              themes and tumbled head first into the rabbit hole of coding and
-              web development. Fast-forward to today, and I’ve had the privilege
-              of building software for an{' '}
-              <a className="text-slate-200 hover:text-teal-300">
-                advertising agency
-              </a>
-              , a <a className="text-slate-200 hover:text-teal-300">start-up</a>{' '}
-              , a{' '}
-              <a className="text-slate-200 hover:text-teal-300">
-                huge corporation
-              </a>
-              , and a{' '}
-              <a className="text-slate-200 hover:text-teal-300">
-                digital product studio
-              </a>
-              .
+              I’m a developer who loves turning ideas into clean, functional,
+              and user-friendly web experiences. Most of my work revolves around{' '}
+              <a className="text-slate-200 hover:text-teal-300">React.js </a>
+              and <a className="text-slate-200 hover:text-teal-300">Next.js</a>,
+              where I build stuff that’s not just nice to look at but also fast,
+              accessible, and easy to use. I’ve worked with teams across the
+              USA, Canada, and Australia, solving real problems and helping
+              businesses connect with their users.
             </p>
             <p className="mb-4">
-              My main focus these days is building accessible user interfaces
-              for our customers at Klaviyo. I most enjoy building software in
-              the sweet spot where design and engineering meet — things that
-              look good but are also built well under the hood. In my free time,
-              I{`'`}ve also released an online video course that covers
-              everything you need to know to build a web app with the Spotify
-              API.
+              Lately, I’ve been diving into the backend world{' '}
+              <a className="text-slate-200">Node.js</a> and{' '}
+              <a className="text-slate-200">PostgreSQL</a>, challenging myself
+              to grow into a full-stack role. Basically, I enjoy learning new
+              things and finding better ways to build.
             </p>
-            <p className="mb-4">
-              When I’m not at the computer, I’m usually rock climbing, reading,
-              hanging out with my wife and two cats, or running around Hyrule
-              searching for Korok seeds.
+            <p className="mb-16">
+              When I’m not coding, you’ll probably find me at the gym ,
+              tinkering with side projects (like a URL shortener app or wedding
+              invites for friends), or geeking out over Next.js and{' '}
+              <a className="text-slate-200">TypeScript</a> . I’m always up for a
+              challenge, whether it’s figuring out a tricky CI/CD pipeline or
+              optimizing some gnarly piece of UI.
             </p>
           </div>
         </section>
 
         <section>
-          <CardContainer>
-            <div className="flex gap-10">
-              <label className="w-3/4 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 ">
-                Aug 2023 - Aug 2024
-              </label>
-              <div className="flex flex-col gap-3">
-                <label className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base">
-                  Next.js Developer | Frontend Engineer
-                </label>
-                <p className="text-slate-500">
-                  Build and maintain critical components used to construct
+          <JobCards
+            dateRange="September - November 2024"
+            jobPosition="Freelance Team Lead"
+            body="Build and maintain critical components used to construct
                   Klaviyo’s frontend, across the whole product. Work closely
                   with cross-functional teams, including developers, designers,
                   and product managers, to implement and advocate for best
-                  practices in web accessibility.
-                </p>
-                <ul className="w-fit flex gap-2 flex-wrap">
-                  <Tags label="JavaScript" />
-                  <Tags label="TypeScript" />
-                  <Tags label="Storybook" />
-                  <Tags label="GraphQL" />
-                  <Tags label="Jest" />
-                </ul>
-              </div>
-            </div>
-          </CardContainer>
+                  practices in web accessibility."
+            tags={['JavaScript', 'TypeScript', 'Code Review', 'Node.js']}
+          />
+
+          <JobCards
+            dateRange="August 2023 - August 2024"
+            jobPosition="Frontend Developer | Next.js Developer"
+            body="Build and maintain critical components used to construct
+                  Klaviyo’s frontend, across the whole product. Work closely
+                  with cross-functional teams, including developers, designers,
+                  and product managers, to implement and advocate for best
+                  practices in web accessibility."
+            tags={['JavaScript', 'TypeScript', 'Storybook', 'GraphQL', 'Jest']}
+          />
         </section>
       </main>
     </div>
